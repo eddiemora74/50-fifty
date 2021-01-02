59fac73d8bf9dfaa72a1a12129ffb2566cfc9cd1
Eddie Mora <eddiemora74@gmail.com>
2021-01-01 23:11:28 -0500

    soundboard

---
+++ 09/app.js
@@ -0,0 +1,20 @@
+const sounds = ["applause", "boo", "gasp", "tada", "victory", "wrong"];
+
+sounds.forEach((sound) => {
+  const btn = document.createElement("button");
+  btn.classList.add("btn");
+  btn.innerText = sound;
+  btn.addEventListener("click", () => {
+    stopSounds();
+    document.getElementById(sound).play();
+  });
+  document.getElementById("buttons").appendChild(btn);
+});
+
+function stopSounds() {
+  sounds.forEach((sound) => {
+    const byte = document.getElementById(sound);
+    byte.pause();
+    byte.currentTime = 0;
+  });
+}
